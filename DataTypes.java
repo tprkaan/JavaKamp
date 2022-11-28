c9c7790c377ff9115dac48a1d11145c201c2d569
tprkaan <113602811+tprkaan@users.noreply.github.com>
2022-11-29 00:03:26 +0300

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+package javademos;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		double sayi = 12.5;
+		sayi = 127;
+
+		char karakter = 'S';
+
+		boolean dogruMu = false;
+
+	}
+
+}
---
+++ Variables.java
@@ -0,0 +1,18 @@
+package javademos;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		int ögrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+
+		System.out.println(mesaj + ögrenciSayisi);
+		System.out.println(mesaj + ögrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ögrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ögrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ögrenciSayisi);
+
+	}
+
+}
